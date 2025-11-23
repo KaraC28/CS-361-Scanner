4c782625a1f24ed9b9d1239e94bc6b76c6b1072a
Kara Cho <chokara037@gmail.com>
2025-11-23 17:20:44 -0500

    updated so it should properly check the prog1.kay file

--- ScannerDemo.java
+++ ScannerDemo.java
@@ -6,15 +6,17 @@
 public class ScannerDemo {
 
 	// This needs to be changed !
-	private static String file1 = "C:\\Users\\cscharff\\eclipse-workspace\\ScannerSolution\\src\\prog1.jay";
+	private static String file1 = "prog1.kay";
 	private static int counter = 1;
 
 	public static void main(String args[]) {
 		TokenStream ts = new TokenStream(file1);
+		Token tk;
 		System.out.println(file1);
 		while (!ts.isEndofFile()) {
 			// TO BE COMPLETED
-			// System.out.println("Token " + counter++ + " - Type: " + tk.getType() + " - Value: " + tk.getValue());
+			tk = ts.nextToken();
+			System.out.println("Token " + counter++ + " - Type: " + tk.getType() + " - Value: " + tk.getValue());
 		}
 	}
 }
